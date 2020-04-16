faf338db5462b255bafea01370c76cfcb896cb3c
Nabeelah <noblenab13@gmail.com>
2020-04-16 10:27:54 +0100

    Apollo server

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 server.listen().then(({ url }) => {
   console.log(`Hello World ${url}`);
